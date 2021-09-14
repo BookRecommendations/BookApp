eba0dd4b5996fbaee12121fd9d47a93d3bdbb7be
DanielRGilles <danryangilles@gmail.com>
2021-09-13 17:44:32 -0700

    added js to app.js to create user

---
+++ app.js
@@ -0,0 +1,14 @@
+import { createUser, setUser } from './utils.js';
+
+// initialize global state
+const formEl = document.getElementById('sign-up');
+
+//submit event listener
+formEl.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(formEl);
+    const user = createUser(formData);
+    setUser(user);
+    window.location = './login/index.html';
+});
